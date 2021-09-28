655961cb994c8a7df17e39164eff0cfa01b4dfe6
Allan Melo <allan-m-silva@hotmail.com>
2021-09-28 17:34:44 -0300

    adiciona teste simples

--- script.test.js
+++ script.test.js
@@ -1,1 +1,9 @@
-const nasa = require('./script');
+const { it, expect, test } = require('@jest/globals');
+const { get } = require('http');
+const { describe } = require('yargs');
+// const { getAPI } = require('./script');
+
+    test('Teste', () => {
+        expect(1).toBe(1);
+    });
+
